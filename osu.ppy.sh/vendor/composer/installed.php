1d1d5475e2bbcd573f1b6b3e46a2848467fa92cf
Minosuko Sylveon <63941519+Minosuko@users.noreply.github.com>
2022-07-14 09:52:42 +0700

    Add files via upload

---
+++ osu.ppy.sh/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '01b50ce984abace3bab8a667027aa979ec3d7feb',
+    'name' => 'osuripple/ripple',
+  ),
+  'versions' => 
+  array (
+    'ezyang/htmlpurifier' => 
+    array (
+      'pretty_version' => 'v4.7.0',
+      'version' => '4.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ae1828d955112356f7677c465f94f7deb7d27a40',
+    ),
+    'osuripple/ripple' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '01b50ce984abace3bab8a667027aa979ec3d7feb',
+    ),
+  ),
+);
